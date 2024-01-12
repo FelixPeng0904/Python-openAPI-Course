432bfc7b2db6c2943c8f96b3046424539888d56e
felixpeng0904 <shionp0904@gmail.com>
2024-01-12 15:14:42 +0800

    renew

--- pico_w/Lesson06_2.py
+++ pico_w/Lesson06_2.py
@@ -9,15 +9,18 @@ led25 = Pin("LED",Pin.OUT)
 ledStatus = False
 
 while True:
-    if time.ticks_diff(time.ticks_ms(),start_ticks1) >= 1000 #var_ticks - start_ticks >= 333:
+    if time.ticks_diff(time.ticks_ms(),start_ticks1) >= 1000:
+        print("pass 1 second")
         start_ticks1 = time.ticks_ms()
         ledStatus = not ledStatus
         led25.value(ledStatus)
-    if time.ticks_diff(time.ticks_ms(),start_ticks1) >= 5000 #var_ticks - start_ticks >= 333:
+    if time.ticks_diff(time.ticks_ms(),start_ticks1) >= 5000:
+        print("pass 5 second")
         start_ticks2 = time.ticks_ms()
         ledStatus = not ledStatus
         led25.value(ledStatus)
-    if time.ticks_diff(time.ticks_ms(),start_ticks1) >= 10000 #var_ticks - start_ticks >= 333:
+    if time.ticks_diff(time.ticks_ms(),start_ticks1) >= 10000:
+        print("pass 10 second")
         start_ticks3 = time.ticks_ms()
         ledStatus = not ledStatus
         led25.value(ledStatus)
