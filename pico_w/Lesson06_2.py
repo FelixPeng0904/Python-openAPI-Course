968ee8d069eefce86e866f21a290dd98aeb7e0e5
felixpeng0904 <shionp0904@gmail.com>
2024-01-12 15:11:25 +0800

    renew

--- pico_w/Lesson06_2.py
+++ pico_w/Lesson06_2.py
@@ -2,13 +2,22 @@ import machine
 import time
 from machine import Pin
 
-start_ticks = time.ticks_ms()
+start_ticks1 = time.ticks_ms()
+start_ticks2 = time.ticks_ms()
+start_ticks3 = time.ticks_ms()
 led25 = Pin("LED",Pin.OUT)
 ledStatus = False
 
 while True:
-    var_ticks = time.ticks_ms()
-    if var_ticks - start_ticks >= 333:
-        start_ticks = time.ticks_ms()
+    if time.ticks_diff(time.ticks_ms(),start_ticks1) >= 1000 #var_ticks - start_ticks >= 333:
+        start_ticks1 = time.ticks_ms()
+        ledStatus = not ledStatus
+        led25.value(ledStatus)
+    if time.ticks_diff(time.ticks_ms(),start_ticks1) >= 5000 #var_ticks - start_ticks >= 333:
+        start_ticks2 = time.ticks_ms()
+        ledStatus = not ledStatus
+        led25.value(ledStatus)
+    if time.ticks_diff(time.ticks_ms(),start_ticks1) >= 10000 #var_ticks - start_ticks >= 333:
+        start_ticks3 = time.ticks_ms()
         ledStatus = not ledStatus
         led25.value(ledStatus)
