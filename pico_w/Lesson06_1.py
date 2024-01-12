d686e98b74b5d71ed00d3b7292eef2e664896975
felixpeng0904 <shionp0904@gmail.com>
2024-01-12 14:24:34 +0800

    led 控制

--- pico_w/Lesson06_1.py
+++ pico_w/Lesson06_1.py
@@ -1,2 +1,14 @@
-name = input("please input your name:")
-print(name)
+import machine
+import time
+from machine import Pin
+
+led25 = Pin("LED", Pin.OUT) #25是一個引數值
+while True:
+    print(machine.freq())
+    led25.value(1) 
+    time.sleep(1)
+    led25.value(0)
+    time.sleep(1)
+    
+    
+    
